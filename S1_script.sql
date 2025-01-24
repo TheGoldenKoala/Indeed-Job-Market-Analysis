31f9675d1516566a4bfb2d6e78e303bea4845693
Juan Casas Doza <154471291+TheGoldenKoala@users.noreply.github.com>
2025-01-24 14:12:04 -0500

    Delete S1_script.sql

--- S1_script.sql
+++
@@ -1,9 +0,0 @@
-SELECT population, county,
-	CASE
-		WHEN population >= 500000 THEN 'high population'
-		WHEN population > 100000 AND population < 500000 THEN 'medium population'
-		WHEN population <= 100000 THEN 'low population'
-		ELSE 'missing'
-	END AS pop_category
-FROM population
-WHERE year = 2017;
